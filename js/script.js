62a5a09df828bd4709f1df65f60cb89f267dd576
puhrome <phirom.yim@fullsail.edu>
2014-05-15 15:06:24 -0400

    Defining Variables
    Setting up basic rules for defining a variable in JS using WebStorm

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/**
+ * Defining Variables Created by Phirom Yim on 5/15/14.
+ */
+//declare
+    //always important to declare before you define
+var age;//always remember to use ; otherwise the browser might put it in the wrong place
+//define
+age = 38; //using = is an assignment operator assigning age as 38
+//spaces is formality, doesn't affect the code, arbitrary
+//storage container = value
+//assignment is from right to left
+//you can declare and define on one line
+
+var yearOfBirth = 1970;//declaring and defining on the same line
